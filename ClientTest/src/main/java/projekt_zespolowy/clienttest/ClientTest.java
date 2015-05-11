8d3c6c67e8469ae3e1c8ef9b931e6b971a314d0c
guncda <guncda@gmail.com>
2015-05-11 04:55:46 +0200

    Wszystko oprócz pkt tokeny

--- ClientTest/src/main/java/projekt_zespolowy/clienttest/ClientTest.java
+++ ClientTest/src/main/java/projekt_zespolowy/clienttest/ClientTest.java
@@ -1,8 +1,12 @@
 package projekt_zespolowy.clienttest;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
@@ -12,6 +16,7 @@ import org.postgresql.geometric.PGpoint;
 
 public class ClientTest
 {
+    
     private String dodajUzytkownika(String email, String haslo, long facebook, long google, String typ) {
         JSONObject json = null;
         try {
@@ -27,7 +32,28 @@ public class ClientTest
             return "Klient: Blad przy tworzeniu JSONa";
         }
 
-        String url_address = "http://localhost:8080/RestApi/service/uzytkownicy/post";
+        String url_address = "http://virt2.iiar.pwr.edu.pl:8080/RestApi/service/uzytkownicy/register";
+
+        String help;
+        help = dataTransfer(json, url_address);
+        return help;
+    }
+    
+    private String login(String email, String haslo, long facebook, long google) {
+        JSONObject json = null;
+        try {
+            json = new JSONObject()
+                .put("uzytkownicy", new JSONObject()
+                    .put("email", email)
+                    .put("haslo", haslo)
+                    .put("facebook", facebook)
+                    .put("google", google)
+            );
+        } catch (JSONException ex) {
+            return "Klient: Blad przy tworzeniu JSONa";
+        }
+
+        String url_address = "http://virt2.iiar.pwr.edu.pl:8080/RestApi/service/uzytkownicy/login";
 
         String help;
         help = dataTransfer(json, url_address);
@@ -90,6 +116,36 @@ public class ClientTest
 
         return print_returned;
     }
+    
+    public static void addZdjecie() {
+        try {
+            URL url = new URL("http://localhost:8084/RestApi/service/zdjecia/post");
+            URLConnection connection = url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "multipart/form-data");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            
+            File file = new File("C:\\Users\\Sebastian\\Desktop\\Nowy folder\\studia\\aa.jpg");
+            byte[] bytes = new byte[(int)file.length()];
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+            bis.read(bytes, 0, bytes.length);
+            OutputStream os = connection.getOutputStream();
+
+            os.write(bytes, 0, bytes.length);
+            os.flush();
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            while (in.readLine() != null) {
+            }
+            System.out.println("\nSukces");
+            in.close();
+        } catch (Exception e) {
+            System.out.println("\nNie przeszlo");
+            System.out.println(e);
+        }
+    }
 
     public static void main(String[] args) {
         ClientTest test = new ClientTest();
@@ -97,9 +153,11 @@ public class ClientTest
         String help;
 
         // TESTY - dokladniejsze informacje o bledach sa wypisywane w oknie serwera
-        //help = test.dodajUzytkownika("przyklad@email.com", "1234", 0, 0, "restapi");
-        help = test.dodajZgloszenie(1, new PGpoint(51.094703, 17.021475), "opis", "email");
+        help = test.dodajUzytkownika("przyklad@email.com", "1234", 0, 0, "restapi");
+        help = test.login("przyklad@email.com", "1234", 0, 0);
+        //help = test.dodajZgloszenie(1, new PGpoint(51.094703, 17.021475), "opis", "email");
 
         System.out.println(help);
+        //addZdjecie();
     }
 }
